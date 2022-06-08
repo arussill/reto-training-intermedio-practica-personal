278fa88bcff1d20e2a19da87bd3b126c4a71baca
Aura Russill <auracris1996@hotmail.com>
2022-06-08 18:40:10 -0500

    INICIO: inicio del proyecto
    copia realizada del repo del reto final de aplicacion empresarial

---
+++ src/main/java/co/com/sofka/questions/collections/Answer.java
@@ -0,0 +1,55 @@
+package co.com.sofka.questions.collections;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+@Data
+@Document(collection = "preguntas")
+public class Answer {
+    @Id
+    private String id;
+    private String userId;
+    private String questionId;
+    private String answer;
+    private Integer position;
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(String questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
