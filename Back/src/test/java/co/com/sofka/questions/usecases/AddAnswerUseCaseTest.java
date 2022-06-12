724e0310c3320d0736fb85cfb63d98e3dafa3817
Aura Russill <auracris1996@hotmail.com>
2022-06-11 23:00:51 -0500

    ADD:AddAnswerUseCaseTest
    se agrego el test de caso de uso de agregar respuesta

--- Back/src/test/java/co/com/sofka/questions/usecases/AddAnswerUseCaseTest.java
+++ Back/src/test/java/co/com/sofka/questions/usecases/AddAnswerUseCaseTest.java
@@ -1,36 +1,73 @@
 package co.com.sofka.questions.usecases;
 
-import co.com.sofka.questions.collections.Answer;
-import co.com.sofka.questions.reposioties.AnswerRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import reactor.core.publisher.Mono;
+        import co.com.sofka.questions.collections.Answer;
+        import co.com.sofka.questions.collections.Question;
+        import co.com.sofka.questions.reposioties.AnswerRepository;
+        import co.com.sofka.questions.reposioties.QuestionRepository;
+        import org.junit.jupiter.api.BeforeEach;
+        import org.junit.jupiter.api.Test;
+        import org.mockito.Mockito;
+        import reactor.core.publisher.Flux;
+        import reactor.core.publisher.Mono;
+        import reactor.test.StepVerifier;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+        import static org.mockito.Mockito.*;
+
 
 class AddAnswerUseCaseTest {
 
+    QuestionRepository questionRepository;
     AnswerRepository answerRepository;
     AddAnswerUseCase addAnswerUseCase;
     GetUseCase getUseCase;
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
         MapperUtils mapperUtils = new MapperUtils();
+        questionRepository = mock(QuestionRepository.class);
         answerRepository = mock(AnswerRepository.class);
+        getUseCase = new GetUseCase(mapperUtils, questionRepository, answerRepository);
         addAnswerUseCase = new AddAnswerUseCase(mapperUtils, getUseCase, answerRepository);
     }
 
     @Test
-    void getValidationTest(){
+    public void addAnswerTest() {
+        MapperUtils mapper = new MapperUtils();
         var answer = new Answer();
-        answer.setId("respuestaId");
-        answer.setUserId("idUsuario");
-        answer.setQuestionId("idPregunta");
+        answer.setId("answerId");
+        answer.setUserId("userId");
+        answer.setQuestionId("questionId");
         answer.setAnswer("respuesta");
-        answer.setPosition(1);
+        answer.setPosition(0);
+
+        var answerdto = mapper.mapEntityToAnswer().apply(answer);
+
+        var question = new Question();
+        question.setId("questionId");
+        question.setUserId("userId");
+        question.setQuestion("pregunta");
+        question.setType("tech");
+        question.setCategory("Software");
+
+        when(questionRepository.findById(any(String.class))).thenReturn(Mono.just(question));
+        when(answerRepository.save(any(Answer.class))).thenReturn(Mono.just(answer));
+        when(answerRepository.findAllByQuestionId(any(String.class))).thenReturn(Flux.empty());
+
+        StepVerifier.create(addAnswerUseCase.apply(answerdto))
+                .expectNextMatches(questiondto -> {
+                    assert questiondto.getId().equals("questionId");
+                    assert questiondto.getUserId().equals("userId");
+                    assert questiondto.getQuestion().equals("pregunta");
+                    assert questiondto.getType().equals("tech");
+                    assert questiondto.getCategory().equals("Software");
+                    assert questiondto.getAnswers().contains(answerdto);
+                    return true;
+                })
+                .verifyComplete();
+
+        verify(questionRepository).findById("answerId");
+
     }
 
-//    when(answerRepository.save(answer)).thenReturn(Mono.just(answer));
+
 }
